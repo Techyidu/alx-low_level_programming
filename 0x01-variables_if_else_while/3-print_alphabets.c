31d0c5e75d5bbff29a27d05d4421dfc40642ff0f
Techyidu <techyidu@gmail.com>
2023-06-19 14:19:16 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char C;
+
+	char d;
+
+	C = 'a';
+	d = 'A';
+	while
+	    (C <= 'z') {
+		    putchar(C);
+C++;
+}
+while
+(d <= 'Z') {
+putchar(d);
+d++;
+	}
+putchar('\n');
+return (0);
+}
