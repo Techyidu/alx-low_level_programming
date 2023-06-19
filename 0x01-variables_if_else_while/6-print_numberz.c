524f8e18234b3a398b6d765023621d824743a6cd
Techyidu <techyidu@gmail.com>
2023-06-19 15:24:44 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int C;
+
+C = 0;
+while
+(C < 10) {
+putchar(C + '0');
+C++;
+		}
+putchar('\n');
+return (0);
+}
