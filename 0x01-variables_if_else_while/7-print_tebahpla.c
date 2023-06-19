ba60b57e16738f91cdd178704618ff4f3faf6581
Techyidu <techyidu@gmail.com>
2023-06-19 15:36:36 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char D;
+
+D = 'z';
+while
+(D >= 'a') {
+putchar(D);
+D--;
+		}
+putchar('\n');
+return (0);
+}
