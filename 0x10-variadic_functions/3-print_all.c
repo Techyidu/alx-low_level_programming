98e019653f7ae78eaaedbad7ac860117a4e6b3e8
Techyidu <techyidu@gmail.com>
2023-07-20 15:00:22 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,14 +10,12 @@
 		int i = 0;
 		char *str, *sep = "";
 va_list list;
-
 va_start(list, format);
-
 if (format)
+	{
+	while (format[i])
 		{
-			while (format[i])
-			{
-				switch (format[i])
+		switch (format[i])
 				{
 					case 'c':
 						printf("%s%c", sep, va_arg(list, int));
